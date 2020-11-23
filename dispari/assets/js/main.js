ceac390ea37a4f87164fdb115e75b4565a3c0230
Romina Trazzi <romina.trazzi@posta.it>
2020-11-23 11:02:56 +0100

    Esercizio pari\dispari senza funzioni 2

--- dispari/assets/js/main.js
+++ dispari/assets/js/main.js
@@ -8,42 +8,46 @@ Dichiariamo chi ha vinto. */
 // Acquisiamo i dati dall'utente
 var userEvenOdds = prompt("Scegli pari o dispari");
 var userChoice = Number(prompt("Scegli un numero da 1 a 5"));
-console.log(userEvenOdds, userChoice);
+// console.log(userEvenOdds, userChoice);
 
 // Controlliamo le scelte dell'utente 
 if (!(isNaN(userEvenOdds))) {
     alert("Non hai inserito pari o dispari, riprova!");
 } else if (isNaN(userChoice)) {
     alert("Non hai inserito un numero, riprova!");
-} /*else if (userChoice = 0 || userChoice > 5); {
-    alert("Il numero inserito è maggiore di 5 o uguale a 0, riprova!");
-} */
+} 
 
 // Generiamo un numero random da 1 a 5 per il computer
 var pcNumber = (Math.floor(Math.random() * 4) + 1);
-console.log(pcNumber);
+// console.log(pcNumber);
 
 // Sommiamo i due numeri
-var sum = userChoice + pcNumber;
+var sum = userChoice + pcNumber; 
 console.log(sum);
 
 // Stabiliamo se la somma dei due numeri è pari o dispari
-if (sum % 2 == 0) {
-    var even = sum;
-} else {
-    var odds = sum;
-} 
+var risultato = sum % 2;
+
+if (risultato == 0) {
+    var risultatoFinale = "pari"
+} else if (risultato != 0) {
+    var risultatoFinale = "dispari"
+}
+// console.log(risultatoFinale);
 
 // Dichiariamo chi ha vinto con un messaggio all'utente
-if (userEvenOdds == "pari" && even == sum) {
-    alert("Il risultato è pari. Hai vinto!");
-} else if (userEvenOdds == "dispari" && even == sum) {
-    alert("Il risultato è pari. Hai perso!");
-} else if (userEvenOdds == "dispari" && odds == sum) {
-    alert("Il risultato è dispari. Hai vinto!"); 
-} else (userEvenOdds == "pari" && odds == sum); {
-    alert("Il risultato è dispari. Hai perso!")
-}
+var msg = "Il risultato è " + risultatoFinale + ".";
+console.log(msg);
+
+if (userEvenOdds == "pari" && risultatoFinale == "pari") {
+    console.log(msg + " Hai vinto!");
+} else if (userEvenOdds == "dispari" && risultatoFinale == "pari") {
+    console.log(msg + " Hai perso!");
+} else if (userEvenOdds == "dispari" && risultatoFinale == "dispari") {
+    console.log(msg + " Hai vinto!"); 
+} else if (userEvenOdds == "pari" && risultatoFinale == "dispari") {
+    console.log(msg + " Hai perso!");
+} 
 
 
 
